d0103bd6bd81916815483211e155b7524bdd2d03
Johann Diedrick <johann@somewheregood.com>
2021-04-15 16:01:46 -0400

    adding sql kit

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0"),
         .package(name: "SwiftJWT", url: "https://github.com/Kitura/Swift-JWT.git", from: "3.0.0")
     ],
     targets: [
